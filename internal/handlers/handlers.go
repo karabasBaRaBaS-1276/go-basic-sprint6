ea133150614f1d47f06f48b51296f1af8bd2f19a
Осенчаков Роман <rongo@yandex.ru>
2025-05-20 19:55:27 +0300

    implemented handler for return index.html

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1,18 +1,52 @@
 package handlers
 
-import "net/http"
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
 
 // Структура, облуживающая запросы на получения статичных данных.
-type StaticHandler struct{}
+type StaticHandler struct {
+	htmlIndexFile string
+}
 
 // NewStaticHandler позволяет получить новый экземпляр [StaticHandler].
 func NewStaticHandler() *StaticHandler {
-	return &StaticHandler{}
+	return &StaticHandler{htmlIndexFile: "index.html"}
 }
 
-func (*StaticHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	http.Error(w, `{"error": "Обработка корневого эндпоинта пока не реализована"}`, http.StatusMethodNotAllowed)
+// Обработка энпоинта '/'
+// Допустим только GET метод
+func (staticHandler *StaticHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+
+	start := time.Now()
+	defer func() {
+		log.Printf("Обработан запрос %s: '%s' (время обработки: %s)\n", r.Method, r.URL.String(), time.Since(start))
+	}()
+
+	if r.Method != http.MethodGet {
+		http.Error(w, fmt.Sprintf("%s method not allowed", r.Method), http.StatusInternalServerError) // хотя больше подходит http.StatusMethodNotAllowed
+	}
+
+	file, err := os.Open(staticHandler.htmlIndexFile)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer file.Close()
+
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	// копируем содержимое файла в ответ
+	_, err = io.Copy(w, file)
+	if err != nil {
+		http.Error(w, "ошибка при отправке файла", http.StatusInternalServerError)
+		return
+	}
+
 }
 
 // Структура, облуживающая запросы по файлам.
